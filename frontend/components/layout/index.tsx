98214a2d5d8c464a211e901e803a7b23da5a6e3a
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2024-10-20 02:21:02 -0700

    wallet done

--- frontend/components/layout/index.tsx
+++ frontend/components/layout/index.tsx
@@ -5,14 +5,22 @@ import { Navigation } from "./navigation";
 import { ProjectsBar } from "./projects-bar";
 import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "../ui/button";
 import { Card, CardContent } from "../ui/card";
-import { shortenAddress } from "@/lib/utils";
+import { getChainImage, shortenAddress } from "@/lib/utils";
+import { Separator } from "../ui/separator";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { polygonAmoy, skaleEuropaTestnet, storyTestnet } from "viem/chains";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const { ready: privyReady, authenticated, user } = usePrivy();
-  const { ready: walletsReady } = useWallets();
+  const { ready: privyReady, authenticated } = usePrivy();
+  const [openWalletPopover, setOpenWalletPopover] = useState(false);
+  const { ready: walletsReady, wallets } = useWallets();
   const pathName = usePathname();
   const router = useRouter();
 
@@ -20,7 +28,11 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     if ((!authenticated || !walletsReady || !privyReady) && pathName != "/") {
       router.push("/");
     }
+    console.log("chainId");
+    console.log(wallets.length > 0 ? wallets[0].chainId : "NO");
   }, [authenticated, walletsReady, privyReady, pathName]);
+
+  useEffect(() => {}, []);
 
   return !privyReady || !walletsReady ? (
     <div className="flex flex-col items-center justify-center h-screen w-screen space-y-4">
@@ -33,7 +45,6 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <ProjectsBar />
       {authenticated && (
         <>
-          {" "}
           <div className="fixed bottom-0 left-0 right-0 z-50">
             <div className="flex justify-center">
               <Navigation />
@@ -41,18 +52,108 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           </div>
           <div className="fixed top-4 right-4 z-50">
             <div className="flex justify-center">
-              <Card className="bg-neutral-900">
-                <CardContent className="flex space-x-2 py-2 px-6 items-center">
-                  <img
-                    src={`https://noun-api.com/beta/pfp?name=${user?.wallet?.address}`}
-                    width={30}
-                    height={30}
-                    alt="nouns_pfp"
-                    className="rounded-full"
-                  />
-                  <p>{shortenAddress(user?.wallet?.address || "")}</p>
-                </CardContent>
-              </Card>
+              <Popover
+                open={openWalletPopover}
+                onOpenChange={setOpenWalletPopover}
+              >
+                <PopoverTrigger asChild>
+                  <Card className="bg-neutral-900 cursor-pointer">
+                    <CardContent className="flex space-x-2 py-0 pl-2 pr-3 items-center">
+                      <Image
+                        src={getChainImage(wallets[0].chainId)}
+                        width={30}
+                        height={30}
+                        alt="avatar"
+                        className="rounded-full my-2"
+                      />
+                      <Separator orientation="vertical" className="h-[44px]" />
+                      <img
+                        src={`https://noun-api.com/beta/pfp?name=${wallets[0].address}`}
+                        width={30}
+                        height={30}
+                        alt="nouns_pfp"
+                        className="rounded-full my-2 "
+                      />
+                      <p className="pr-1">
+                        {shortenAddress(wallets[0].address || "")}
+                      </p>
+                    </CardContent>
+                  </Card>
+                </PopoverTrigger>
+                <PopoverContent className="bg-neutral-900 w-[200px]">
+                  <div className="flex flex-col space-y-1">
+                    <Button
+                      variant={
+                        wallets[0].chainId == `eip155:${skaleEuropaTestnet.id}`
+                          ? "default"
+                          : "ghost"
+                      }
+                      onClick={() => {
+                        wallets[0].switchChain(skaleEuropaTestnet.id);
+                        setOpenWalletPopover(false);
+                      }}
+                      className="flex space-x-2 justify-center"
+                    >
+                      <Image
+                        src="/chains/skale.png"
+                        width={20}
+                        height={20}
+                        alt="skale"
+                      />
+                      <p>SKALE</p>
+                    </Button>
+                    <Button
+                      variant={
+                        wallets[0].chainId == `eip155:${polygonAmoy.id}`
+                          ? "default"
+                          : "ghost"
+                      }
+                      onClick={() => {
+                        wallets[0].switchChain(polygonAmoy.id);
+                        setOpenWalletPopover(false);
+                      }}
+                      className="flex space-x-2 justify-center"
+                    >
+                      <Image
+                        src="/chains/polygon.png"
+                        width={20}
+                        height={20}
+                        alt="polygon"
+                      />
+                      <p>Polygon</p>
+                    </Button>
+                    <Button
+                      variant={
+                        wallets[0].chainId == `eip155:${storyTestnet.id}`
+                          ? "default"
+                          : "ghost"
+                      }
+                      onClick={() => {
+                        wallets[0].switchChain(storyTestnet.id);
+                        setOpenWalletPopover(false);
+                      }}
+                      className="flex space-x-2 justify-center"
+                    >
+                      <Image
+                        src="/chains/story.png"
+                        width={20}
+                        height={20}
+                        alt="story"
+                      />
+                      <p>Story</p>
+                    </Button>
+                    <Button
+                      variant={"secondary"}
+                      onClick={() => {
+                        wallets[0].disconnect();
+                        setOpenWalletPopover(false);
+                      }}
+                    >
+                      Disconnect
+                    </Button>
+                  </div>
+                </PopoverContent>
+              </Popover>
             </div>
           </div>
         </>
--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -1,5 +1,6 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { polygonAmoy, skaleEuropaTestnet, storyTestnet } from "viem/chains";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -16,4 +17,16 @@ export function formattedNumber(num: number): string {
 }
 export function shortenAddress(address: string): string {
   return `${address.slice(0, 6)}...${address.slice(-4)}`;
+}
+export function getChainImage(chainId: string): string {
+  switch (chainId) {
+    case `eip155:${skaleEuropaTestnet.id}`:
+      return "/chains/skale.png";
+    case `eip155:${storyTestnet.id}`:
+      return "/chains/story.png";
+    case `eip155:${polygonAmoy.id}`:
+      return "/chains/polygon.png";
+    default:
+      return "/logo-nouns.png";
+  }
 }
